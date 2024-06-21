f90eac646206a40f2bd327d1cbdaf55b72f1937d
Gergő Takács <spiroka01@gmail.com>
2024-06-21 21:32:13 +0200

    Update vite.config.ts

--- packages/note-taking-app/vite.config.ts
+++ packages/note-taking-app/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../../build/note-taking-app'
+  },
   resolve: {
     alias: {
       "@features": path.resolve(__dirname, "src/features"),
